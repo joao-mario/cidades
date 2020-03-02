c0b6a6dfc837d930186d1cf4afb16bb109b73418
joao mario <jjsp0403@gmail.com>
2020-03-02 18:41:03 -0300

    adicionando o exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mongagua");
+            Console.WriteLine("itanhanhem");
+            Console.WriteLine("peruibe");
+        }
+    }
+}
